b55ec308179438c21ec424e5a80808646fdfa7f7
Daniel Wijono <danielwijono999@gmail.com>
2018-05-13 20:16:06 +0700

    fix signup

--- app/src/main/java/com/example/daniel/homehelp/SignUpActivity.java
+++ app/src/main/java/com/example/daniel/homehelp/SignUpActivity.java
@@ -1,13 +1,36 @@
 package com.example.daniel.homehelp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    @BindView(R.id.et_name)
+    EditText etName;
+    @BindView(R.id.et_phone_number)
+    EditText etPhoneNumber;
+    @BindView(R.id.et_email)
+    EditText etEmail;
+    @BindView(R.id.tv_sign_up)
+    TextView tvSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.tv_sign_up)
+    public void onViewClicked() {
+        startActivity(new Intent(this, DashBoardActivity.class));
+        finish();
     }
 }
