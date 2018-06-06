4ba53828158a21fcbe91df51fd3ad6acf9be9f59
Daniel Wijono <danielwijono999@gmail.com>
2018-06-06 23:32:28 +0700

    add find worker and choose worker activity

---
+++ app/src/main/java/com/example/daniel/homehelp/activity/ChooseWorkerActivity.java
@@ -0,0 +1,15 @@
+package com.example.daniel.homehelp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.daniel.homehelp.R;
+
+public class ChooseWorkerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_worker);
+    }
+}
--- app/src/main/java/com/example/daniel/homehelp/activity/FindingWorkerActivity.java
+++ app/src/main/java/com/example/daniel/homehelp/activity/FindingWorkerActivity.java
@@ -1,9 +1,12 @@
 package com.example.daniel.homehelp.activity;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.daniel.homehelp.R;
+import com.example.daniel.homehelp.Utils;
 
 public class FindingWorkerActivity extends AppCompatActivity {
 
@@ -11,5 +14,14 @@ public class FindingWorkerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finding_worker);
+        delayHandler();
+    }
+
+    private void delayHandler() {
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(FindingWorkerActivity.this, ChooseWorkerActivity.class));
+            }
+        }, 3000);
     }
 }
