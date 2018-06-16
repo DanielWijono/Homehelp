34b4ccbec6583661d00eacfc9d0db5d33c954402
Daniel Wijono <danielwijono999@gmail.com>
2018-06-16 17:46:55 +0700

    add on click butterknife in tracking worker

--- app/src/main/java/com/example/daniel/homehelp/activity/TrackingWorkerActivity.java
+++ app/src/main/java/com/example/daniel/homehelp/activity/TrackingWorkerActivity.java
@@ -3,13 +3,16 @@ package com.example.daniel.homehelp.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.daniel.homehelp.R;
 import com.example.daniel.homehelp.Utils;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class TrackingWorkerActivity extends AppCompatActivity {
 
@@ -17,6 +20,14 @@ public class TrackingWorkerActivity extends AppCompatActivity {
     Toolbar toolbar;
     @BindView(R.id.ll_info)
     LinearLayout llInfo;
+    @BindView(R.id.tv_title_toolbar)
+    TextView tvTitleToolbar;
+    @BindView(R.id.ll_chat)
+    LinearLayout llChat;
+    @BindView(R.id.ll_telephone)
+    LinearLayout llTelephone;
+    @BindView(R.id.tv_cancel)
+    TextView tvCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +35,17 @@ public class TrackingWorkerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_tracking_worker);
         ButterKnife.bind(this);
         Utils.setupAppToolbarForActivity(this, toolbar, "Tracking Pekerja");
+    }
+
+    @OnClick({R.id.ll_chat, R.id.ll_telephone, R.id.tv_cancel})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.ll_chat:
+                break;
+            case R.id.ll_telephone:
+                break;
+            case R.id.tv_cancel:
+                break;
+        }
     }
 }
