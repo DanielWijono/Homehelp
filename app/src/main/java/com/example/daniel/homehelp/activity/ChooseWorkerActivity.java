bb93cc1dc4f4a2e966cd83607b7f53a887df7b92
Daniel Wijono <danielwijono999@gmail.com>
2018-06-16 20:32:27 +0700

    fix choose worker ui

--- app/src/main/java/com/example/daniel/homehelp/activity/ChooseWorkerActivity.java
+++ app/src/main/java/com/example/daniel/homehelp/activity/ChooseWorkerActivity.java
@@ -4,29 +4,61 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.daniel.homehelp.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class ChooseWorkerActivity extends AppCompatActivity {
 
     @BindView(R.id.tv_order_button)
     TextView tvOrderButton;
+    @BindView(R.id.img_check_worker_one)
+    ImageView imgCheckWorkerOne;
+    @BindView(R.id.ll_worker_one)
+    LinearLayout llWorkerOne;
+    @BindView(R.id.img_check_worker_two)
+    ImageView imgCheckWorkerTwo;
+    @BindView(R.id.ll_worker_two)
+    LinearLayout llWorkerTwo;
+
+    boolean isOneClicked, isTwoClicked;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_worker);
         ButterKnife.bind(this);
+    }
 
-        tvOrderButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(ChooseWorkerActivity.this, TrackingWorkerActivity.class));
-            }
-        });
+    @OnClick({R.id.ll_worker_one, R.id.ll_worker_two, R.id.tv_order_button})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.ll_worker_one:
+                isOneClicked = true;
+                isTwoClicked = false;
+                imgCheckWorkerOne.setImageResource(R.drawable.ic_already_choose);
+                imgCheckWorkerTwo.setImageResource(R.drawable.ic_not_choose);
+                break;
+            case R.id.ll_worker_two:
+                isOneClicked = false;
+                isTwoClicked = true;
+                imgCheckWorkerOne.setImageResource(R.drawable.ic_not_choose);
+                imgCheckWorkerTwo.setImageResource(R.drawable.ic_already_choose);
+                break;
+            case R.id.tv_order_button:
+                if (isOneClicked || isTwoClicked ) {
+                    startActivity(new Intent(ChooseWorkerActivity.this, TrackingWorkerActivity.class));
+                } else {
+                    Toast.makeText(this, "Mohon pilih salah satu pekerja", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
     }
 }
