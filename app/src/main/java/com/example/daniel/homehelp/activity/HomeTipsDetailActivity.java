434d11b1a080b1a2e8a4864c21a2a19a15c79833
Daniel Wijono <danielwijono999@gmail.com>
2018-07-01 21:30:12 +0700

    add new images for asset in tips detail

---
+++ app/src/main/java/com/example/daniel/homehelp/activity/HomeTipsDetailActivity.java
@@ -0,0 +1,13 @@
+package com.example.daniel.homehelp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeTipsDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_tips_detail);
+    }
+}
